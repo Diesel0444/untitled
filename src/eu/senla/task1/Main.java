2172fcdbd587386df2965fa4fe44bc44e5da2965
Diesel <Diesel04@Yandex.ru>
2021-01-29 12:57:50 +0300

    Initial commit

---
+++ src/eu/senla/task1/Main.java
@@ -0,0 +1,38 @@
+package eu.senla.task1;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args){
+        Number number = new Number();
+        number.setA((byte) 122);
+        byte a = number.getA();
+        System.out.println(a);
+        int s = 12;
+        byte b = 17;
+        Integer i = 200;
+        Integer in = 100;
+        byte f = (byte) s;
+        System.out.println(f+" dsdsd");
+
+
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(in);
+        System.out.println(i);
+
+        String x = "kfkdfm";
+        System.out.println(x);
+        Character e = 's';
+        System.out.println(e);
+
+        Boolean bool = true;
+        System.out.println(bool);
+
+
+    }
+
+
+}
